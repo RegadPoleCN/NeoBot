0222961b0ed912f8a4ca58fe51aefdc269e14726
Aurelian2842 <aurelian2842@gmail.com>
2025-12-06 22:35:48 +0800

    fix: velocity didn't call NeoBot#enable()

--- velocity/src/main/java/dev/neovoxel/neobot/NeoBotVelocity.java
+++ velocity/src/main/java/dev/neovoxel/neobot/NeoBotVelocity.java
@@ -1,6 +1,8 @@
 package dev.neovoxel.neobot;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
@@ -77,6 +79,11 @@ public class NeoBotVelocity implements NeoBot {
         this.proxyServer = proxyServer;
         this.logger = logger;
         this.dataDirectory = dataDirectory;
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        this.enable();
     }
 
     @HostAccess.Export
